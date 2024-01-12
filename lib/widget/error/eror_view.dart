ddd03f2bd37f71196c1d9cf682a46404979cdc1e
Víctor Alba <victoram97@gmail.com>
2024-01-12 20:12:38 +0100

    add optional onCancel function to eror dialog

--- lib/widget/error/eror_view.dart
+++ lib/widget/error/eror_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ErrorView {
-  static show(BuildContext context, String message, Function onRetry) {
+  static show(BuildContext context, String message, Function onRetry,
+      {Function? onCancel}) {
     showDialog(
       context: context,
       builder: (dialogContext) {
@@ -12,6 +13,9 @@ class ErrorView {
           actions: [
             TextButton(
               onPressed: () {
+                if (onCancel != null) {
+                  onCancel.call();
+                }
                 Navigator.of(dialogContext).pop();
               },
               child: const Text("Cancel"),
